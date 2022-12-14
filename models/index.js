4a5a1be3ddcc2bfc21b7be06d87632e485f6e41e
Natalia Venkatesh <nataliavenkatesh@macbook-pro.lan>
2022-12-14 18:28:41 -0500

    added member model

---
+++ models/index.js
@@ -0,0 +1,19 @@
+const Member = require ('./member')
+const Class = require('./class');
+const Instructor = require('./instructor');
+
+Member.hasMany(Class, {
+  foreignKey: 'member_id',
+  onDelete: 'CASCADE'
+});
+
+Class.hasMany(Member, {
+  foreignKey: 'member_id'
+});
+
+Class.belongsTo(Intsructor, {
+    foreignKey: 'instructor_id'
+  });
+
+
+module.exports = { Member, Class, Instructor};
---
+++ models/member.js
@@ -0,0 +1,63 @@
+const { Model, DataTypes } = require('sequelize');
+const bcrypt = require('bcrypt');
+const sequelize = require('../config/connection');
+
+class Member extends Model {
+  checkPassword(loginPw) {
+    return bcrypt.compareSync(loginPw, this.password);
+  }
+}
+
+Member.init(
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      first_name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      last_name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+        validate: {
+          isEmail: true,
+        },
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          len: [8],
+        },
+      },
+    },
+    {
+        hooks: {
+          beforeCreate: async (newMemberData) => {
+            newMemberData.password = await bcrypt.hash(newMemberData.password, 10);
+            return newMemberData;
+          },
+          beforeUpdate: async (updatedMemberData) => {
+            updatedMemberData.password = await bcrypt.hash(updatedMemberData.password, 10);
+            return updatedMemberData;
+          },
+        },
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'member',
+      }
+    );
+    
+    module.exports = User;
+    
