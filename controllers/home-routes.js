7bd5b38abe59be819dc3ba7f80b79d318e7e8874
Natalia Venkatesh <nataliavenkatesh@macbook-pro.lan>
2022-12-20 12:22:14 -0500

    added activity seed, model and add a route to homeroutes file. added code for login screen

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,21 +1,53 @@
 const router = require('express').Router();
-const {Instructor} = require('../models')
+const { Instructor } = require('../models')
 
 router.get('/test', (req, res) => {
     res.send('working');
 })
 
+//routing to homepage
+router.get ('/', async (req,res)=> {
+    res.render('index');
+})
+
+//routing to About us
+router.get('/aboutus', (req, res) => {
+    res.render('page2-aboutus');
+});
+
+
+//routing to contact us page
 router.get('/contact-us', (req, res) => {
     res.render('page5-contactus');
 })
-router.get('/instrList', async(req, res) => {
-  const instructorResults = await Instructor.findAll({
 
-  })
-  const instructors = instructorResults.map(value => value.get({plain:true}))
+//routing to instructors
+router.get('/instrList', async (req, res) => {
+    const instructorResults = await Instructor.findAll({
+
+    })
+    const instructors = instructorResults.map(value => value.get({ plain: true }))
     return res.render('instrList', {
-      instructors: instructors
-    }); 
-   
-  });
+        instructors: instructors
+    });
+
+});
+//routing to all activities
+router.get('/activlist', async (req, res) => {
+    const activityResults = await Activity.findAll({
+
+    })
+    const activities = activityResults.map(value => value.get({ plain: true }))
+    return res.render('activlist', {
+        activities: activities
+    });
+
+});
+//routing to login/signup page
+router.get('/login', (req, res) => {
+    res.render('login');
+});
+
+
 module.exports = router;
+
