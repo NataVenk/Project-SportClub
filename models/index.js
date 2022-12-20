9be686edd05af01d29a179fc001e9ad52ffe3c84
Natalia Venkatesh <nataliavenkatesh@macbook-pro.lan>
2022-12-20 16:49:01 -0500

    commented out new code

--- models/index.js
+++ models/index.js
@@ -1,18 +1,18 @@
 const Member = require ('./member')
 const Activity = require('./activity');
 const Instructor = require('./instructor');
-const MemberActivity = require('./member-activities');
+// const MemberActivity = require('./member-activities');
 
-Member.hasMany(Activity, {
-  through: MemberActivity, 
-  foreignKey: 'member_id',
+// Member.hasMany(Activity, {
+//   through: MemberActivity, 
+//   foreignKey: 'member_id',
   
-});
+// });
 
-Activity.hasMany(Member, {
-  through: MemberActivity, 
-  foreignKey: 'activity_id'
-});
+// Activity.hasMany(Member, {
+//   through: MemberActivity, 
+//   foreignKey: 'activity_id'
+// });
 
 Activity.belongsTo(Instructor, {
     foreignKey: 'instructor_id'
