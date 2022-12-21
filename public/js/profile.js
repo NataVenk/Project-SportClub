28608e0a3e3d71fd4b8b249c6f04210a34756810
Natalia Venkatesh <nataliavenkatesh@macbook-pro.lan>
2022-12-21 15:56:39 -0500

    signup function works - need soem tweaking with submit button functionality

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,40 +1,29 @@
 const newActivity = async (event) => {
     event.preventDefault();
-  
-    const name = document.querySelector('#member_name').value.trim();
-    const member_activity = document.querySelector('#member-activity').value.trim();
+  const checks = document.querySelectorAll('input[type="checkbox"]:checked')
+  const activityIds = []
+  checks.forEach(check => activityIds.push(parseInt(check.value)))
+  console.log(activityIds)
+    // const name = document.querySelector('#member_name').value.trim();
+    // const member_activity = document.querySelector('#member-activity').value.trim();
     
   
-    if (name && member_activity) {
-      const response = await fetch(`/yourschedule`, {
+    if (activityIds.length) {
+      const response = await fetch(`/api/activity`, {
         method: 'POST',
-        body: JSON.stringify({ name, activity_id }),
+        body: JSON.stringify(activityIds),
         
       });
   
       if (response.ok) {
         document.location.replace('/profile');
       } else {
-        alert('Failed to create project');
-      }
-    }
-  };
-  
-  const delButtonHandler = async (event) => {
-    if (event.target.hasAttribute('data-id')) {
-      const id = event.target.getAttribute('data-id');
-  
-      const response = await fetch(`/api/projects/${id}`, {
-        method: 'DELETE',
-      });
-  
-      if (response.ok) {
-        document.location.replace('/profile');
-      } else {
-        alert('Failed to delete project');
+        alert('Failed to update');
       }
     }
   };
 
+    
+  document.querySelector(".interest-form").addEventListener("submit", newActivity)
   
   
